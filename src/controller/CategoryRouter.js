e0a2b9fcfc96c5cfe0078f4f18adc9d4d0d9e0d6
parithoshjenfvj <gudiparithosh@gmail.com>
2026-03-21 18:13:00 +0530

    mongodb added

--- src/controller/CategoryRouter.js
+++ src/controller/CategoryRouter.js
@@ -1,5 +1,7 @@
 let express=require("express")
 let categoryRouter=express.Router()
+const mongodb=require('mongodb').MongoClient;
+let url=process.env.MONGO_URL
 category=[
     {
         "id":1,
@@ -26,7 +28,22 @@ category=[
 function router(menu){
     categoryRouter.route('/')
     .get((req,res)=>{
-        res.render('category',{title:'Category Page',category,menu})
+        mongodb.connect(url,function(err,dc){
+            if (err){
+                res.status(500).send("Error while connecting")
+            }
+            else{
+                let dbObj=dc.db('aprnode');
+                dbObj.collection('category').find().toArray(function(err,category){
+                    if (err){
+                        res.status(500).send("Error while connecting")
+                    }
+                    else{
+                        res.render('category',{title:'Category Page',category,menu})
+                    }
+                })
+            }
+        })
     })
     categoryRouter.route('/details')
     .get((req,res)=>{
