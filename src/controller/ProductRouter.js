ba88a0824d28466b02bb33af07ab06c67379214b
parithoshjenfvj <gudiparithosh@gmail.com>
2026-03-23 18:33:00 +0530

    added params

--- src/controller/ProductRouter.js
+++ src/controller/ProductRouter.js
@@ -23,6 +23,41 @@ function router(menu){
             }
         })
     })
+
+    productRouter.route("/category/:id")
+    .get((req,res)=>{
+        // let id=req.params.id
+        let {id}=req.params
+        mongodb.connect(url,function(err,dc){
+            let dbObj=dc.db('aprnode');
+            dbObj.collection('products').find({category_id:Number(id)}).toArray(function(err,products){
+                if (err){
+                    res.status(500).send("Error while connecting")
+                }
+                else{
+                    res.render('products',{title:'products Page',products,menu})
+                }
+            })
+        })
+    })
+
+    productRouter.route("/:id")
+    .get((req,res)=>{
+        let {id}=req.params
+        mongodb.connect(url,function(err,dc){
+            let dbObj=dc.db('aprnode');
+            dbObj.collection('products').find({id:Number(id)}).toArray(function(err,products){
+                if (err){
+                    res.status(500).send("Error while connecting")
+                }
+                else{
+                    res.send(products)
+                    // res.render('products',{title:'products Page',products,menu})
+                }
+            })
+        })
+    })
+
     productRouter.route('/details')
     .get((req,res)=>{
         res.send("products details")
